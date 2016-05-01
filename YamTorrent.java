7f553a0f46c431c7ce064cc7c37d0741b561439b
dherg <hergendavid@gmail.com>
2016-05-01 16:46:18 -0400

    init

---
+++ YamTorrent.java
@@ -0,0 +1,16 @@
+/* 	YamTorrent.java
+*
+* 	David Hergenroeder (david.hergenroeder@yale.edu)
+* 
+*
+*/
+
+class YamTorrent {
+
+	int main(String args[]) {
+
+		System.out.println("YamTorrent");
+		
+	} // end of main
+
+} // end of class YamTorrent
