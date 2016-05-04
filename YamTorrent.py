30d138e4ba249b7911a00b3354dc4deb43b1148a
dherg <hergendavid@gmail.com>
2016-05-04 00:32:58 -0400

    trying to get test hashing working

--- YamTorrent.py
+++ YamTorrent.py
@@ -1,5 +1,7 @@
 import sys
 import bencode
+import requests
+import hashlib
 
 def main():
 
@@ -8,7 +10,22 @@ def main():
 
 	# dictionary of torrent file
 	torrentdict = bencode.bdecode(torrentfile)
-	print(torrentdict)
+
+	# re-bencode the info section
+	info = torrentdict.get("info")
+	bencodedinfo = bencode.bencode(info)
+	# print(bencodedinfo)
+
+	# SHA1 hash of info section
+	sha1 = hashlib.sha1(bencodedinfo)
+	infohash = sha1.digest()
+	# print(type(bencodedinfo))
+	for char in infohash:
+		print(hex(ord(char)))
+
+
+
+	# print(infohash)
 
 if __name__ == "__main__":
 	main()
