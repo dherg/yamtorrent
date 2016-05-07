dccf8fc2b8cc734616ad60b7dd2f055a01b56c00
Micah Rosales <micah.rosales@gmail.com>
2016-05-07 13:36:23 -0400

    Update syntax to use spaces and single-quotes

--- YamTorrent.py
+++ YamTorrent.py
@@ -1,84 +1,79 @@
+#!/usr/bin/env python3
 import sys
-# import bencode
 import requests
 import hashlib
 import bencodepy
 
+
 def main():
+    # open file in binary
+    try:
+        torrentfile = open(sys.argv[1], "rb").read()
+    except IOError:
+        print("BAD FILE NAME: " + sys.argv[1])
+        exit()
 
-	# open file in binary
-	try:
-		torrentfile = open(sys.argv[1], "rb").read()
-	except IOError:
-		print("BAD FILE NAME: " + sys.argv[1])
-		exit()
+        print("BEGINNING")
 
-	print("BEGINNING")
+    # dictionary of torrent file
+    # torrentdict = bencode.bdecode(torrentfile)
+    torrentdict = bencodepy.decode(torrentfile)
+    # print(torrentdict)
+    # print(type(torrentdict))
 
-	# dictionary of torrent file
-	# torrentdict = bencode.bdecode(torrentfile)
-	torrentdict = bencodepy.decode(torrentfile)
-	# print(torrentdict)
-	# print(type(torrentdict))
+    # re-bencode the info section
+    info = torrentdict[b"info"]
+    # print(info)
+    bencodedinfo = bencodepy.encode(info)
+    # print(info)
+    # print(bencodedinfo)
 
-	# re-bencode the info section
-	info = torrentdict[b"info"]
-	# print(info)
-	bencodedinfo = bencodepy.encode(info)
-	# print(info)
-	# print(bencodedinfo)
+    #COMPUTE PARAMETERS FOR ANNOUNCE
 
+    # SHA1 hash of info section
+    sha1 = hashlib.sha1(bencodedinfo)
+    info_hash = sha1.digest()
+    # print(type(bencodedinfo))
+    # for char in info_hash:
+    #   print(hex(char))
+    #   print(char)
 
+    peer_id = (hashlib.sha1(b"0")).digest()
+    port = b'6881'
+    uploaded = b'0'
+    downloaded = b'0'
 
-	#COMPUTE PARAMETERS FOR ANNOUNCE
+    try:
+        left = 0
+        for f in info[b'files']:
+            left += f[b'length']
+    except KeyError:
+        left = info[b'length']
 
-	# SHA1 hash of info section
-	sha1 = hashlib.sha1(bencodedinfo)
-	info_hash = sha1.digest()
-	# print(type(bencodedinfo))
-	# for char in info_hash:
-	# 	print(hex(char))
-	# 	print(char)
+    compact = b'1'
+    event = b'started'
 
-	peer_id = (hashlib.sha1(b"0")).digest()
-	port = "6881"
-	uploaded = "0"
-	downloaded = "0"
+    url = torrentdict[b'announce']
 
-	try:
-		left = 0
-		for f in info[b"files"]:
-			left += f[b"length"]
-	except KeyError:
-		left = info[b"length"]
+    p = {'info_hash': info_hash, 'peer_id': peer_id, 'port': port, 'uploaded': uploaded, 'downloaded': downloaded, 'left': left, 'compact': compact, 'event': event}
+    r = requests.get(url.decode(), params=p)
 
-	compact = "1"
-	event = "started"
+    # print(info_hash)
+    # print(bencodedinfo)
 
-	url = torrentdict[b"announce"]
+    # with open("temp.txt",'wb') as f:
+    #   f.write(r.text.encode())
 
+    print(r.text)
+    print(r.url)
+    print(r.text[0])
+    print(r.text[362])
+    print('CONTENT')
+    print(r.content)
+    print('END CONTENT')
 
-	p = {"info_hash": info_hash, "peer_id": peer_id, "port": port, "uploaded": uploaded, "downloaded": downloaded, "left": left, "compact": compact, "event": event}
-	r = requests.get(url.decode(), params=p)
+    response = bencodepy.decode(r.content)
+    print(response)
 
-	# print(info_hash)
-	# print(bencodedinfo)
-
-	# with open("temp.txt",'wb') as f:
-	# 	f.write(r.text.encode())
-
-	print(r.text)
-	print(r.url)
-	print(r.text[0])
-	print(r.text[362])
-	print('CONTENT')
-	print(r.content)
-	print('END CONTENT')
-
-	#this doesn't work
-	response = bencodepy.decode(r.content)
-	print(response)
-
-
-if __name__ == "__main__":
-	main()
+    if __name__ == '__main__':
+        main()
