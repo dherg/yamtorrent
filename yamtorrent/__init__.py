b8c02d6a8575be0d8bc8319c23cac9cce5b05e81
Lincoln Swaine-Moore <lswainemoore@gmail.com>
2016-05-07 22:03:15 -0400

    removed ip int

--- yamtorrent/__init__.py
+++ yamtorrent/__init__.py
@@ -2,9 +2,8 @@ from .peerconnection import PeerConnection
 
 
 class PeerInfo:
-    def __init__(self, ip, ip_int, port):
+    def __init__(self, ip, port):
         self.ip = ip
-        self.ip_int = ip_int
         self.port = port
 
     def __str__(self):
