bd6dfa9c5a185d6745c2c61150dc310f5f8b9fb5
Lincoln Swaine-Moore <lswainemoore@gmail.com>
2016-05-08 01:22:08 -0400

    buffer and all that entails

--- yamtorrent/peerconnection.py
+++ yamtorrent/peerconnection.py
@@ -1,3 +1,4 @@
+import sys
 import struct
 from twisted.internet.defer import Deferred
 from twisted.internet.protocol import ClientFactory
@@ -12,8 +13,7 @@ class PeerConnection(object):
     class _States(Enum):
         WAIT_CONNECT = 0
         WAIT_HANDSHAKE = 1
-        WAIT_BITFIELD = 2
-        WIRE = 3
+        LATER = 2
 
     def __init__(self, meta, peer_info, protocol=None):
         self.meta = meta
@@ -21,6 +21,7 @@ class PeerConnection(object):
         self.bitfield = None
         self.state = self._States.WAIT_CONNECT
         self._protocol = protocol
+        self.buf = bytearray()
 
     def connect(self, reactor):
         self.done = Deferred()
@@ -38,33 +39,173 @@ class PeerConnection(object):
 
     def rcv_handshake(self, data):
         print('rcv_handshake:', data)
+        print(len(data))
         pass
 
-    def request_bitfield(self):
+    # NOT USING RIGHT NOW
+    def send_bitfield(self):
         # construct message
         msg = bytearray(2)
         # self.transport.write(msg)
-        self.state = self._States.WAIT_BITFIELD
+        # self.state = self._States.WAIT_BITFIELD
 
-    def rcv_bitfield(self, data):
+    def rcv_keepalive(self):
+        print('rcv_keepalive')
+        # we probably should reset a timer or something
+        pass
+
+    def rcv_choke(self, msg, msg_length):
+        print('rcv_choke', msg_length)
+        pass
+
+    def rcv_unchoke(self, msg, msg_length):
+        print('rcv_unchoke', msg_length)
+        pass
+
+    def rcv_interested(self, msg, msg_length):
+        print('rcv_interested', msg_length)
+        pass
+
+    def rcv_notinterested(self, msg, msg_length):
+        print('rcv_notinterested', msg_length)
+        pass
+
+    def rcv_have(self, msg, msg_length):
+        print('rcv_hav', msg_length)
+        pass
+
+    def rcv_bitfield(self, msg, msg_length):
         # parse bitfield
         # store bitfield locally
-        # print('rcv_bitfield:', data)
+        print('rcv_bitfield:', msg)
+        # message_length = struct.unpack(">i",msg[:4])[0]
+        # print('length', message_length)
+        # print(len(msg))
+        # print(msg[0])
+        # print(msg[1])
+        # print(msg[2])
+        # print(msg[3])
+        # print(msg[4])
+        # print(msg[0:5])
+        # print(len(msg[0:5]))
         bitfield = ()
         # self.done.callback(bitfield)
+
+    def rcv_request(self, msg, msg_length):
+        print('rcv_request', msg_length)
+        pass
+
+    def rcv_piece(self, msg, msg_length):
+        print('rcv_piece', msg_length)
+        pass
+
+    def rcv_cancel(self, msg, msg_length):
+        print('rcv_cancel', msg_length)
+        pass
+
+    def rcv_port(self, msg, msg_length):
+        print('rcv_port', msg_length)
+        pass
+
+
+    def process_buf(self):
+
+
+        #if this should a received handshake
+        if (len(self.buf) > 0) and (len(self.buf) >= 49 + self.buf[0]) and (self.state == self._States.WAIT_HANDSHAKE):
+            
+            pstrlen = self.buf[0]
+            pstr = self.buf[1:pstrlen + 1]
+            reserved = self.buf[pstrlen + 1:pstrlen + 9]
+            info_hash = self.buf[pstrlen + 9:pstrlen+29]
+            peer_id = self.buf[pstrlen+29:pstrlen+49]
+
+            #check that this is the handshake receipt
+            if (pstr == b"BitTorrent protocol") and (info_hash == self.meta.info_hash):
+                
+                self.state = self._States.LATER
+                data = self.buf[:pstrlen+49]
+
+                self.buf = self.buf[pstrlen+49:]
+                print('handshake match.')
+
+                return data,0
+
+            else:
+                print('PROBABLY SHOULD DROP CONNECTION B/C BAD HANDSHAKE')
+
+        
+
+
+        #if we don't even have a length at the start
+        if len(self.buf) < 4:
+            return None,0
+
+        msg_length = struct.unpack("!i",self.buf[:4])[0]
+
+        print('process_buf len=', len(self.buf), 'want=', msg_length, 'type = ', self.buf[4])
+
+
+        #if we have the full message in the buffer
+        if len(self.buf) >= 4 + msg_length:
+            data = self.buf[:msg_length+4]
+
+            #remove message from buffer
+            self.buf = self.buf[msg_length+4:]
+
+            return data,msg_length
+
+        return None,0
+
+    def handle_message(self, msg, msg_length):
+
+        # if this is a keep-alive
+        if msg_length == 0:
+            self.rcv_keepalive()
+            return None
+
+        msg_type = msg[4]
+
+        options = {
+            0: self.rcv_choke,
+            1: self.rcv_unchoke,
+            2: self.rcv_interested,
+            3: self.rcv_notinterested,
+            4: self.rcv_have,
+            5: self.rcv_bitfield,
+            6: self.rcv_request,
+            7: self.rcv_piece,
+            8: self.rcv_cancel,
+            9: self.rcv_port
+        }
+
+        options[msg_type](msg, msg_length)
+
+
+
+
+
 
     # this is called async by your event loop
     def rx_data(self, data):
 
-        # parse data here
-        if self.state == self._States.WAIT_HANDSHAKE:
-            self.rcv_handshake(data)
-            self.request_bitfield()
-        elif self.state == self._States.WAIT_BITFIELD:
-            self.rcv_bitfield(data)
-        else:
-            # unknown message
-            pass
+        self.buf += data
+
+        next_message, msg_length = self.process_buf()
+        print(next_message, msg_length)
+
+        if next_message is not None:
+            self.handle_message(next_message, msg_length)
+
+        # # parse data here
+        # if self.state == self._States.WAIT_HANDSHAKE:
+        #     self.rcv_handshake(data)
+        #     self.request_bitfield()
+        # elif self.state == self._States.WAIT_BITFIELD:
+        #     self.rcv_bitfield(data)
+        # else:
+        #     # unknown message
+        #     pass
 
     def did_connect(self, protocol):
         self._protocol = protocol
